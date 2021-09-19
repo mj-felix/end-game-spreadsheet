5f20097fcd31fcc78370bef01e6b9f3cfacd429b
MJ Felix <mjfelixdev@gmail.com>
2021-09-20 10:59:56 +1200

    Add JSDoc

--- js/app.js
+++ js/app.js
@@ -19,70 +19,177 @@ class Utils {
     }
 }
 
+/** Class representing a Cell of data. */
 class Cell {
-    constructor(id, type = 'input', value = null, formula = null) {
+    /**
+     * Creates a Cell.
+     * @constructor
+     * @param {string} id Cell id; concatenated identifier of a column and row, e.g. A2.
+     * @param {string} type Cell type; 'label' denotes header cell, 'input' denotes cell holding non-label value.
+     * @param {string} value Cell value; input entered into the cell for 'input' cells that do not start with =, label for 'label' cells.
+     */
+    constructor(id, type = 'input', value = null) {
+        /**
+         * Cell id; concatenated identifier of a column and row, e.g. A2.
+         * @type {string}
+         */
         this.id = id;
+        /**
+         * Cell type; 'label' denotes header cell, 'input' denotes cell holding non-label value.
+         * @type {string}
+         */
         this.type = type;
+        /**
+         * Cell value; input entered into the cell for 'input' cells that do not start with =, label for 'label' cells.
+         * @type {string}
+         */
         this.value = value;
-        this.formula = formula;
+        /**
+         * Cell formula; input entered into the cell starting with =.
+         * @type {string}
+         */
+        this.formula = null;
+        /**
+         * Ids of cells that use this cell in their formulas.
+         * @type {Array.<string>}
+         */
         this.impactedCellIds = [];
+        /**
+         * Ids of cells that this cell uses to calculate its value basing on its formula.
+         * @type {Array.<string>}
+         */
         this.reliantOnCellIds = [];
+        /**
+         * Bold flag; denotes if the value of the cell should be presented in bold in UI.
+         * @type {boolean}
+         */
         this.isBold = false;
+        /**
+         * Italic flag; denotes if the value of the cell should be presented in italic in UI.
+         * @type {boolean}
+         */
         this.isItalic = false;
+        /**
+         * Underlined flag; denotes if the value of the cell should be presented as underlined in UI.
+         * @type {boolean}
+         */
         this.isUnderlined = false;
     }
 
+    /**
+    *  Returns Cell id.
+    *
+    *  @returns {string} Cell id.
+    */
     getId() {
         return this.id;
     }
 
+    /**
+    *  Returns Cell value.
+    *
+    *  @returns {string} Cell value.
+    */
     getValue() {
         return this.value;
     }
 
+    /**
+    *  Sets Cell value.
+    *
+    *  @param {string} value Cell value.
+    */
     setValue(value) {
         this.value = value;
     }
 
+    /**
+    *  Returns Cell folrmula.
+    *
+    *  @returns {string} Cell formula.
+    */
     getFormula() {
         return this.formula;
     }
 
+    /**
+    *  Sets Cell formula.
+    *
+    *  @param {string} formula Cell formula.
+    */
     setFormula(formula) {
         this.formula = formula && formula.toUpperCase();
     }
 
+    /**
+    *  Returns Bold flag.
+    *
+    *  @returns {boolean} Bold flag.
+    */
     getIsBold() {
         return this.isBold;
     }
 
+    /**
+    *  Toggles Bold flag and returns new value of Bold flag.
+    *
+    *  @returns {boolean} Bold flag.
+    */
     toggleIsBold() {
         this.isBold = !this.isBold;
         return this.isBold;
     }
 
+    /**
+    *  Returns Italic flag.
+    *
+    *  @returns {boolean} Italic flag.
+    */
     getIsItalic() {
         return this.isItalic;
     }
 
+    /**
+    *  Toggles Italic flag and returns new value of Italic flag.
+    *
+    *  @returns {boolean} Italic flag.
+    */
     toggleIsItalic() {
         this.isItalic = !this.isItalic;
         return this.isItalic;
     }
 
+    /**
+    *  Returns Underlined flag.
+    *
+    *  @returns {boolean} Underlined flag.
+    */
     getIsUnderlined() {
         return this.isUnderlined;
     }
 
+    /**
+    *  Toggles Underlined flag and returns new value of Underlined flag.
+    *
+    *  @returns {boolean} Underlined flag.
+    */
     toggleIsUnderlined() {
         this.isUnderlined = !this.isUnderlined;
         return this.isUnderlined;
     }
 
+    /**
+    *  Adds Cell id into Impacted Cell Ids array.
+    *
+    *  @param {string} otherCellId Cell id.
+    */
     addImpactedCell(otherCellId) {
         this.impactedCellIds.push(otherCellId);
     }
 
+    /**
+    *  Calculates and stores value of the cell when formula inputted is a sum.
+    */
     updateValueFromSumFormula() {
         let marginalCells = this.formula.slice(5);
         marginalCells = marginalCells.substring(0, marginalCells.length - 1);
@@ -115,6 +222,9 @@ class Cell {
         }
     }
 
+    /**
+    *  Calculates and stores value of the cell when formula inputted consists of only addition, subtraction, mutiplication, division operators and round parentheses and cell ids.
+    */
     updateValueFromRegularFormula() {
         let operation = this.formula.slice(1);
         const cellIds = operation.split(/[+\-*/()]/);
@@ -150,6 +260,9 @@ class Cell {
         }
     }
 
+    /**
+    *  Calculates and stores value of the cell when formula inputted .
+    */
     updateValueFromFormula() {
         if (this.formula.startsWith('=SUM')) {
             this.updateValueFromSumFormula();
@@ -159,9 +272,20 @@ class Cell {
     }
 }
 
+/** Class representing a Spreadsheet of Cells (Model). */
 class Spreadsheet {
+    /**
+     * Creates a Spreadsheet.
+     * @constructor
+     * @param {number} numOfColumns Number of columns for the spreadsheet.
+     * @param {number} numOfRows Number of rows for the spreadsheet.
+     */
     constructor(numOfColumns, numOfRows) {
-        this.data = [];
+        /**
+         * Data object holding properties (indicating columns of spreadsheet) of arrays of Cells.
+         * @type {object}
+         */
+        this.data = {};
 
         const firstColumn = [];
         firstColumn.push(new Cell('0', 'label', ''));
@@ -182,14 +306,32 @@ class Spreadsheet {
 
     }
 
+    /**
+    *  Returns Data object.
+    *
+    *  @returns {object} Data object.
+    */
     getData() {
         return this.data;
     }
 
+    /**
+    *  Returns Cell object.
+    *
+    *  @param {string} column Spreadsheet column identifier.
+    *  @param {string} row Spreadsheet row identifier.
+    *  @returns {Cell} Cell object.
+    */
     getCell(column, row) {
         return this.data[column][row];
     }
 
+    /**
+    *  Returns Cell object.
+    *
+    *  @param {string} columnRow Cell id.
+    *  @returns {Cell} Cell object.
+    */
     getCellById(columnRow) {
         const index = columnRow.search(/\d/);
         const column = columnRow.slice(0, index);
@@ -197,10 +339,21 @@ class Spreadsheet {
         return this.getCell(column, row);
     }
 
+    /**
+    *  Returns spreadsheet column identifiers.
+    *
+    *  @returns {Array.<string>} Array of spreadsheet column identifiers.
+    */
     getColumns() {
         return Object.keys(this.data);
     }
 
+    /**
+    *  Returns object consisting of: first row, last row, first column index, last column index (as restricted by provided 2 cell ids) and array of spreadsheet column identifiers.
+    *
+    *  @param {Array.<string>} marginalCells Array of 2 cell ids.
+    *  @returns {object} Object.
+    */
     getMarginalCellsForRange(marginalCells) {
 
         const index1 = marginalCells[0].search(/\d/);
@@ -237,19 +390,43 @@ class Spreadsheet {
 
 }
 
+/** Class representing a Painter (View). */
 class Painter {
+    /**
+     * Creates a Painter.
+     * @constructor
+     */
     constructor() {
+        /**
+         * Active (focused on) cell id.
+         * @type {string}
+         */
         this.activeCellId = null;
     }
 
+    /**
+    *  Returns active cell id.
+    *
+    *  @returns {string} Cell id.
+    */
     getActiveCellId() {
         return this.activeCellId;
     }
 
+    /**
+    *  Sets active cell id.
+    *
+    *  @param {string} cellId Cell id.
+    */
     setActiveCellId(cellId) {
         this.activeCellId = cellId;
     }
 
+    /**
+    *  Initializes the spreadsheet: paints it and adds various listeners to listen to user actions.
+    *
+    *  @param {Spreadsheet} spreadsheet Spreadsheet of data.
+    */
     init(spreadsheet) {
         this.paint(spreadsheet);
         this.addRefreshListener(spreadsheet);
@@ -259,6 +436,11 @@ class Painter {
         this.addSetInactiveCellListener();
     }
 
+    /**
+    *  Adds listener to user clicking refresh button.
+    *
+    *  @param {Spreadsheet} spreadsheet Spreadsheet of data.
+    */
     addRefreshListener(spreadsheet) {
         document.querySelector('#refreshButton').addEventListener('click', (event) => {
             document.getElementById('spreadsheet').innerHTML = 'Refreshing ...';
@@ -266,6 +448,9 @@ class Painter {
         });
     }
 
+    /**
+    *  Adds listener to user clicking bold button.
+    */
     addBoldListener() {
         document.querySelector('#boldButton').addEventListener('click', (event) => {
             const activeCellId = this.getActiveCellId();
@@ -276,6 +461,9 @@ class Painter {
         });
     }
 
+    /**
+    *  Adds listener to user clicking italic button.
+    */
     addItalicListener() {
         document.querySelector('#italicButton').addEventListener('click', (event) => {
             const activeCellId = this.getActiveCellId();
@@ -286,6 +474,9 @@ class Painter {
         });
     }
 
+    /**
+    *  Adds listener to user clicking underline button.
+    */
     addUnderlineListener() {
         document.querySelector('#underlineButton').addEventListener('click', (event) => {
             const activeCellId = this.getActiveCellId();
@@ -296,6 +487,9 @@ class Painter {
         });
     }
 
+    /**
+    *  Adds listener to user clicking outside of spreadsheet and not bold, italic, underline button to set active cell id to null.
+    */
     addSetInactiveCellListener() {
         document.body.addEventListener('click', (event) => {
             if (!event.target.classList.contains('cell')
@@ -308,6 +502,11 @@ class Painter {
         });
     }
 
+    /**
+    *  Paints the spreadsheet.
+    *
+    *  @param {Spreadsheet} spreadsheet Spreadsheet of data.
+    */
     paint(spreadsheet) {
 
         const data = spreadsheet.getData();
@@ -339,6 +538,11 @@ class Painter {
         this.listen(spreadsheet);
     }
 
+    /**
+    *  Adds cells listeners.
+    *
+    *  @param {Spreadsheet} spreadsheet Spreadsheet of data.
+    */
     listen(spreadsheet) {
 
         const cells = document.querySelectorAll('input.cell');
@@ -361,6 +565,12 @@ class Painter {
 
     }
 
+    /**
+    *  Moves cursor to cell below or skips to the top of next column.
+    *
+    *  @param {object} event Event triggered.
+    *  @param {Spreadsheet} spreadsheet Spreadsheet of data.
+    */
     moveCursorToCellBelow(event, spreadsheet) {
 
         const cellId = event.target.id;
@@ -389,6 +599,12 @@ class Painter {
 
     }
 
+    /**
+    *  Makes cell bold in UI.
+    *
+    *  @param {string} cellId Cell id.
+    *  @param {boolean} isBold Bold flag.
+    */
     setBold(cellId, isBold) {
         const inputClassList = document.getElementById(cellId).classList;
         if (isBold) {
@@ -398,6 +614,12 @@ class Painter {
         }
     }
 
+    /**
+    *  Makes cell italic in UI.
+    *
+    *  @param {string} cellId Cell id.
+    *  @param {boolean} isItalic Italic flag.
+    */
     setItalic(cellId, isItalic) {
         const inputClassList = document.getElementById(cellId).classList;
         if (isItalic) {
@@ -407,6 +629,12 @@ class Painter {
         }
     }
 
+    /**
+    *  Makes cell underlined in UI.
+    *
+    *  @param {string} cellId Cell id.
+    *  @param {boolean} isUnderlined Underlined flag.
+    */
     setUnderlined(cellId, isUnderlined) {
         const inputClassList = document.getElementById(cellId).classList;
         if (isUnderlined) {
@@ -416,39 +644,82 @@ class Painter {
         }
     }
 
+    /**
+    *  Updates cell in UI to present new value.
+    *
+    *  @param {string} cellId Cell id.
+    *  @param {string} newInputValue New input value.
+    */
     updateInput(cellId, newInputValue) {
         document.getElementById(cellId).value = newInputValue;
     }
 
 }
 
+/** Class representing a Controller. */
 class Controller {
+    /**
+     * Creates a Controller.
+     * @constructor
+     * @param {Spreadsheet} spreadsheet Spreadsheet of data.
+     */
     constructor(spreadsheet) {
+        /**
+         * Spreadsheet of data
+         * @type {Spreadsheet}
+         */
         this.spreadsheet = spreadsheet;
     }
 
+    /**
+    *  Returns Spreadsheet of data.
+    *
+    *  @returns {Spreadsheet} Spreadsheet of data.
+    */
     getSpreadsheet() {
         return this.spreadsheet;
     }
 
+    /**
+    *  Toggles cell's bold flag and updates UI accordignly.
+    *
+    *  @param {string} cellId Cell id.
+    */
     toggleBold(cellId) {
         const cell = this.spreadsheet.getCellById(cellId);
         const isBold = cell.toggleIsBold();
         painter.setBold(cellId, isBold);
     }
 
+    /**
+    *  Toggles cell's italic flag and updates UI accordignly.
+    *
+    *  @param {string} cellId Cell id.
+    */
     toggleItalic(cellId) {
         const cell = this.spreadsheet.getCellById(cellId);
         const isItalic = cell.toggleIsItalic();
         painter.setItalic(cellId, isItalic);
     }
 
+    /**
+    *  Toggles cell's underlined flag and updates UI accordignly.
+    *
+    *  @param {string} cellId Cell id.
+    */
     toggleUnderlined(cellId) {
         const cell = this.spreadsheet.getCellById(cellId);
         const isUnderlined = cell.toggleIsUnderlined();
         painter.setUnderlined(cellId, isUnderlined);
     }
 
+    /**
+    *  Saves the cell's new value or formula and, if cell is defocused with formula entered, calculates the result basing on that formula; updates other cells impacted by the change in this cell.
+    *
+    *  @param {string} cellId Cell id.
+    *  @param {string} value Cell id.
+    *  @param {boolean} isBlurEvent Cell id.
+    */
     saveCell(cellId, value, isBlurEvent) {
         const cell = this.spreadsheet.getCellById(cellId);
 
@@ -478,6 +749,11 @@ class Controller {
 
     }
 
+    /**
+    *  Updates cells impacted by the cell with cell id passed in.
+    *
+    *  @param {string} cellId Cell id.
+    */
     updateImpactedCells(cell) {
         for (let impactedCellId of cell.impactedCellIds) {
             // removed after introduction of cell.impactedCellIds
@@ -494,6 +770,11 @@ class Controller {
         }
     }
 
+    /**
+    *  Check if cell holds a formula and updates UI accordingly.
+    *
+    *  @param {string} cellId Cell id.
+    */
     checkForFormula(cellId) {
         painter.setActiveCellId(cellId);
 
